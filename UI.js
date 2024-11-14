9fcd44ba1cc8bf8df5ef8366cb424322805c8698
James Brierley <james.brierley@ibm.com>
2024-11-14 10:37:01 +0000

    clean up selector code

--- UI.js
+++ UI.js
@@ -18,73 +18,77 @@ class UI {
 		return tone;
 	}
 
-	static getDco() {
-		
-		const saw = document.querySelector('input[name=saw_type]:checked');
-		const pulse = document.querySelector('input[name=pulse_type]:checked');
-		const sub = document.querySelector('input[name=sub_type]:checked');
-		const dco_env_mode = document.getElementById('dco_env_mode');
-		const dco_env_amt = document.getElementById('dco_env_amt');
-		const dco_lfo_amt = document.getElementById('dco_lfo_amt');
-		const dco_pw = document.getElementById('dco_pw');
-		const dco_pwm_rate = document.getElementById('dco_pwm_rate');
-		const dco_aft = document.getElementById('dco_aft');
-		const dco_range = document.getElementById('range');
-		const sub_level = document.getElementById('sub_level');
-		const noise_level = document.getElementById('noise_level');
+	static getParams(params) {
 
-		const dco = {
-			'DCO_ENV_MODE': parseInt(dco_env_mode.value),
-			'DCO_WAVEFORM_PULSE': parseInt(pulse.value),
-			'DCO_WAVEFORM_SAWTOOTH': parseInt(saw.value),
-			'DCO_WAVEFORM_SUB': parseInt(sub.value),
-			'DCO_RANGE': parseInt(dco_range.value),
-			'DCO_SUB_LEVEL': parseInt(sub_level.value),
-			'DCO_NOISE_LEVEL': parseInt(noise_level.value),
-			'DCO_LFO_MOD_DEPTH': parseInt(dco_lfo_amt.value),
-			'DCO_ENV_MOD_DEPTH': parseInt(dco_env_amt.value),
-			'DCO_AFTER_DEPTH': parseInt(dco_aft.value),
-			'DCO_PW_PWM_DEPTH': parseInt(dco_pw.value),
-			'DCO_PWM_RATE': parseInt(dco_pwm_rate.value)
-		};
+		const values = {};
+
+		for (let param of params) {
+			values[param] = parseInt(document.getElementById(param).value);
+		}
+
+		return values;
+	}
+
+	static getDco() {
+
+		const dcoParams = [
+			'DCO_ENV_MODE',
+			'DCO_RANGE',
+			'DCO_SUB_LEVEL',
+			'DCO_NOISE_LEVEL',
+			'DCO_LFO_MOD_DEPTH',
+			'DCO_ENV_MOD_DEPTH',
+			'DCO_AFTER_DEPTH',
+			'DCO_PW_PWM_DEPTH',
+			'DCO_PWM_RATE',
+			'BENDER_RANGE'
+		];
+
+		const dco = this.getParams(dcoParams);
+
+		const waveformParams = [
+			'DCO_WAVEFORM_PULSE',
+			'DCO_WAVEFORM_SAWTOOTH',
+			'DCO_WAVEFORM_SUB'
+		];
+
+		for (let waveformParam of waveformParams) {
+			dco[waveformParam] = parseInt(
+				document.querySelector(`input[name=${waveformParam}]:checked`).value
+			);
+		}
 
 		console.log(dco);
 		return dco;
 	}
 
 	static getLfo() {
-		const rate = document.getElementById('lfo_rate');
-		const delay = document.getElementById('lfo_delay');
 
-		const lfo = {
-			'LFO_RATE': parseInt(rate.value),
-			'LFO_DELAY_TIME': parseInt(delay.value)
-		};
+		const lfoParams = [
+			'LFO_RATE',
+			'LFO_DELAY_TIME'
+		];
+
+		const lfo = this.getParams(lfoParams);
 
 		console.log(lfo);
 		return lfo;
 	}
 
 	static getVcf() {
-		const hpf = document.getElementById('hpf');
-		const cutoff = document.getElementById('cutoff');
-		const res = document.getElementById('res');
-		const lfo_amt =  document.getElementById('vcf_lfo_amt');
-		const env_amt = document.getElementById('vcf_env_amt');
-		const env_mode = document.getElementById('vcf_env_mode');
-		const kybd = document.getElementById('vcf_kybd');
-		const after = document.getElementById('vcf_after');
 
-		const filter = {
-			'HPF_CUTOFF_FREQ': parseInt(hpf.value),
-			'VCF_CUTOFF_FREQ': parseInt(cutoff.value),
-			'VCF_RESONANCE': parseInt(res.value),
-			'VCF_LFO_MOD_DEPTH': parseInt(lfo_amt.value),
-			'VCF_ENV_MOD_DEPTH': parseInt(env_amt.value),
-			'VCF_ENV_MODE': parseInt(env_mode.value),
-			'VCF_KEY_FOLLOW': parseInt(kybd.value),
-			'VCF_AFTER': parseInt(after.value)
-		};
+		const vcfParams = [
+			'HPF_CUTOFF_FREQ',
+			'VCF_CUTOFF_FREQ',
+			'VCF_RESONANCE',
+			'VCF_LFO_MOD_DEPTH',
+			'VCF_ENV_MOD_DEPTH',
+			'VCF_ENV_MODE',
+			'VCF_KEY_FOLLOW',
+			'VCF_AFTER_DEPTH'
+		];
+
+		const filter = this.getParams(vcfParams);
 
 		console.log(filter);
 		return filter;
@@ -92,25 +96,18 @@ class UI {
 
 	static getEnvelope() {
 
-		const t1 = document.getElementById('t1');
-		const l1 = document.getElementById('l1');
-		const t2 = document.getElementById('t2');
-		const l2 = document.getElementById('l2');
-		const t3 = document.getElementById('t3');
-		const l3 = document.getElementById('l3');
-		const t4 = document.getElementById('t4');
-		const kybd = document.getElementById('env_kybd');
+		let envParams = [
+			'ENV_T1',
+			'ENV_L1',
+			'ENV_T2',
+			'ENV_L2',
+			'ENV_T3',
+			'ENV_L3',
+			'ENV_T4',
+			'ENV_KEY_FOLLOW'
+		];
 
-		const env = {
-			'ENV_T1': parseInt(t1.value),
-			'ENV_L1': parseInt(l1.value),
-			'ENV_T2': parseInt(t2.value),
-			'ENV_L2': parseInt(l2.value),
-			'ENV_T3': parseInt(t3.value),
-			'ENV_L3': parseInt(l3.value),
-			'ENV_T4': parseInt(t4.value),
-			'ENV_KEY_FOLLOW': parseInt(kybd.value)
-		};
+		const env = this.getParams(envParams);
 
 		console.log(env);
 		return env;
@@ -118,13 +115,10 @@ class UI {
 
 	static getChorus() {
 
-		const on = document.getElementById('chorus_on');
-		const rate = document.getElementById('chorus_rate');
+		const chorus = {};
 
-		const chorus = {
-			'CHORUS': on.checked,
-			'CHORUS_RATE': parseInt(rate.value)
-		};
+		chorus['CHORUS'] = document.getElementById('CHORUS').checked;
+		chorus['CHORUS_RATE'] = parseInt(document.getElementById('CHORUS_RATE').value);
 
 		console.log(chorus);
 		return chorus;
